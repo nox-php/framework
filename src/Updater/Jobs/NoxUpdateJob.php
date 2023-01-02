dba0dcf5976fce2815c677de63536029dc8e5572
Kieron <sutton.kieron99@gmail.com>
2023-01-02 04:49:12 +0000

    Initial commit

--- src/Updater/Jobs/NoxUpdateJob.php
+++ src/Updater/Jobs/NoxUpdateJob.php
@@ -3,16 +3,18 @@
 namespace Nox\Framework\Updater\Jobs;
 
 use Composer\InstalledVersions;
+use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\URL;
 use Nox\Framework\Auth\Models\User;
 use Nox\Framework\Support\Composer;
 
-class NoxUpdateJob implements ShouldQueue
+class NoxUpdateJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable;
 
@@ -40,6 +42,12 @@ class NoxUpdateJob implements ShouldQueue
                 ->danger()
                 ->title('Nox has unsuccessfully updated')
                 ->body('Nox ' . $this->version . ' has failed to install, reverting back to ' . $currentVersion)
+                ->actions([
+                    Action::make('update-nox-retry')
+                        ->button()
+                        ->label('Retry')
+                        ->url(URL::signedRoute('nox.updater', ['version' => $this->version]))
+                ])
                 ->sendToDatabase($this->user);
         }
     }
