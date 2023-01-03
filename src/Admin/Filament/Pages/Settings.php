dd7eec1523d4c226d57b8ef59bbf161044029137
Kieron <sutton.kieron99@gmail.com>
2023-01-03 08:03:22 +0000

    Initial commit

--- src/Admin/Filament/Pages/Settings.php
+++ src/Admin/Filament/Pages/Settings.php
@@ -2,7 +2,14 @@
 
 namespace Nox\Framework\Admin\Filament\Pages;
 
+use Closure;
 use Filament\Facades\Filament;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Pages\Actions\Action;
 use Filament\Pages\Page;
@@ -24,7 +31,31 @@ class Settings extends Page
 
     public function mount(): void
     {
-        $this->form->fill();
+        $databaseConfig = collect(config('database.connections.' . config('database.default'), []))
+            ->only([
+                'driver',
+                'host',
+                'port',
+                'database',
+                'username'
+            ])
+            ->mapWithKeys(static fn($value, $key): array => [
+                'database_' . $key => $value
+            ])
+            ->all();
+
+        $state = [
+            'site_name' => config('app.name'),
+            'site_url' => config('app.url'),
+            'site_environment' => config('app.env'),
+            'site_debug' => config('app.debug'),
+
+            ...$databaseConfig,
+
+            'discord_client_id' => config('nox.auth.discord.client_id'),
+        ];
+
+        $this->form->fill($state);
     }
 
     protected function getViewData(): array
@@ -67,6 +98,135 @@ class Settings extends Page
                 ->label('Check for updates')
                 ->button()
                 ->action('checkUpdate')
+        ];
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Tabs::make('Settings')
+                ->disableLabel()
+                ->tabs([
+                    Tabs\Tab::make('Site')
+                        ->schema([
+                            Fieldset::make('Global')
+                                ->schema([
+                                    TextInput::make('site_name')
+                                        ->label('Site name')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('site_url')
+                                        ->required()
+                                        ->maxLength(255)
+                                ]),
+                            Fieldset::make('Debugging')
+                                ->schema([
+                                    Select::make('site_environment')
+                                        ->label('Environment')
+                                        ->required()
+                                        ->options([
+                                            'production' => 'Production',
+                                            'testing' => 'Testing',
+                                            'local' => 'Local'
+                                        ]),
+                                    Toggle::make('site_debug')
+                                        ->label('Enable debug mode')
+                                        ->helperText('This should never be enabled in production')
+                                ])
+                        ]),
+                    Tabs\Tab::make('Database')
+                        ->schema([
+                            Grid::make()
+                                ->schema([
+                                    Select::make('database_driver')
+                                        ->label('Driver')
+                                        ->reactive()
+                                        ->required()
+                                        ->options([
+                                            'mysql' => 'mysql',
+                                            'pgsql' => 'pgsql',
+                                            'sqlsrv' => 'sqlsrv',
+                                            'sqlite' => 'sqlite'
+                                        ]),
+                                    TextInput::make('database_host')
+                                        ->label('Host')
+                                        ->required(static fn(Closure $get): bool => $get('database_driver') !== 'sqlite')
+                                        ->hidden(static fn(Closure $get): bool => $get('database_driver') === 'sqlite'),
+                                    TextInput::make('database_port')
+                                        ->label('Port')
+                                        ->integer()
+                                        ->minValue(1)
+                                        ->required(static fn(Closure $get): bool => $get('database_driver') !== 'sqlite')
+                                        ->hidden(static fn(Closure $get): bool => $get('database_driver') === 'sqlite'),
+                                    TextInput::make('database_database')
+                                        ->label('Database')
+                                        ->required(),
+                                    TextInput::make('database_username')
+                                        ->label('Username')
+                                        ->required(static fn(Closure $get): bool => $get('database_driver') !== 'sqlite')
+                                        ->hidden(static fn(Closure $get): bool => $get('database_driver') === 'sqlite'),
+                                    TextInput::make('database_password')
+                                        ->label('Password')
+                                        ->password()
+                                        ->hidden(static fn(Closure $get): bool => $get('database_driver') === 'sqlite'),
+                                ])
+                        ]),
+                    Tabs\Tab::make('Discord')
+                        ->schema([
+                            Grid::make()
+                                ->schema([
+                                    TextInput::make('discord_client_id')
+                                        ->label('Client ID')
+                                        ->required(),
+                                    TextInput::make('discord_client_secret')
+                                        ->label('Client secret')
+                                        ->required(static fn(): bool => config('nox.auth.discord.client_secret') === null)
+                                        ->password()
+                                ])
+                        ]),
+                    Tabs\Tab::make('Email')
+                        ->schema([
+                            Grid::make()
+                                ->schema([
+                                    Select::make('email_driver')
+                                        ->label('Driver')
+                                        ->reactive()
+                                        ->required()
+                                        ->options([
+                                            'smtp' => 'smtp',
+                                            'sendmail' => 'sendmail'
+                                        ]),
+                                    TextInput::make('path')
+                                        ->label('Path')
+                                        ->default('/usr/sbin/sendmail -bs -i')
+                                        ->required(static fn(Closure $get): bool => $get('email_driver') === 'sendmail')
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') !== 'sendmail'),
+                                    Select::make('email_host')
+                                        ->label('Host')
+                                        ->required(static fn(Closure $get): bool => $get('email_driver') !== 'sendmail')
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') === 'sendmail'),
+                                    TextInput::make('email_port')
+                                        ->label('Port')
+                                        ->integer()
+                                        ->minValue(1)
+                                        ->required(static fn(Closure $get): bool => $get('email_driver') !== 'sendmail')
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') === 'sendmail'),
+                                    TextInput::make('email_username')
+                                        ->label('Username')
+                                        ->required(static fn(Closure $get): bool => $get('email_driver') !== 'sendmail')
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') === 'sendmail'),
+                                    TextInput::make('email_password')
+                                        ->label('Password')
+                                        ->password()
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') === 'sendmail'),
+                                    TextInput::make('email_encryption')
+                                        ->label('Encryption')
+                                        ->required(static fn(Closure $get): bool => $get('email_driver') !== 'sendmail')
+                                        ->hidden(static fn(Closure $get): bool => $get('email_driver') === 'sendmail')
+                                        ->default('tls')
+                                ])
+                        ])
+                ])
         ];
     }
 }
