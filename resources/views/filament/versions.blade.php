fed92fbf5912db0697fd71ffd243f211b029130c
Kieron <sutton.kieron99@gmail.com>
2023-01-03 04:41:58 +0000

    Initial commit

--- resources/views/filament/versions.blade.php
+++ resources/views/filament/versions.blade.php
@@ -4,7 +4,7 @@
 ])
      x-data
      x-show="$store.sidebar.isOpen">
-    <ul class="flex flex-wrap items-center gap-x-4 gap-y-2">
+    <ul class="flex flex-wrap items-center justify-between gap-x-4 gap-y-2">
         <li class="flex-shrink-0">Nox {{ $versions['nox'] }}</li>
         <li class="flex-shrink-0">PHP v{{ $versions['php'] }}</li>
     </ul>
