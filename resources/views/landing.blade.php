96f4ecd96a86be8e4835a0d79bb5f4f4a1acd8a4
Kieron <sutton.kieron99@gmail.com>
2023-01-04 11:44:55 +0000

    Initial commit

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,5 @@
 <x-filament::layouts.app :title="$title">
-    <h1 class="text-8xl font-bold font-sans text-primary-600 dark:text-primary-500">
+    <h1 class="text-8xl font-bold font-sans text-primary-600 dark:text-primary-500 motion-safe:animate-pulse">
         {{ $title }}
     </h1>
 </x-filament::layouts.app>
